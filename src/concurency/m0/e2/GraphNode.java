330b94d9df0bffeec279f6b5e1d006a7b796015e
Roman <romavr99@gmail.com>
2018-04-11 22:17:18 +0300

    add package Concurrency.m0

---
+++ src/concurency/m0/e2/GraphNode.java
@@ -0,0 +1,23 @@
+package concurency.m0.e2;
+
+import java.util.Set;
+
+public class GraphNode {
+	
+	private final XOField node;
+	
+	private final Set<GraphNode> children;
+	
+	public GraphNode(final XOField node, Set<GraphNode> children) {
+		this.node = node;
+		this.children = children;
+	}
+	
+	public XOField getNode() {
+		return this.node;
+	}
+	
+	public Set<GraphNode> getChildren() {
+		return this.children;
+	}
+}
---
+++ src/concurency/m0/e3/Main.java
@@ -0,0 +1,8 @@
+package concurency.m0.e3;
+
+public class Main {
+
+	public static void main(String...strings) {
+		
+	}
+}
