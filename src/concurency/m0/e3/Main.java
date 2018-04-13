be117c16aa6de38902871d185af8df691e1a5155
r.vorobyev <romakf99@yandex.ru>
2018-04-13 14:58:40 +0300

    Create GraphHelper in m0.e3 and main

--- src/concurency/m0/e3/Main.java
+++ src/concurency/m0/e3/Main.java
@@ -1,8 +1,14 @@
 package concurency.m0.e3;
 
+import concurency.m0.e2.GraphNode;
+import concurency.m0.e2.XOField;
+
 public class Main {
 
 	public static void main(String...strings) {
-		
+		final GraphNode root = new GraphBuilder().build(XOField.Figure.X, new XOField());
+		//System.out.println(root.getNode());
+		//GraphHelper.show(root, 0);
+		System.out.println(GraphHelper.countNodes(root));
 	}
 }
